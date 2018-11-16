005bc3b5f6b9e712286927e78b1527316417eec8
Oksana <xenos2000@ukr.net>
2018-11-15 22:55:46 -0500

    Update Editor
    change Editor to stateless component

--- src/js/components/Editor.js
+++ src/js/components/Editor.js
@@ -1,39 +1,34 @@
 import React from 'react';
 
-export default class Editor extends React.Component {
+const Editor = (props) => {
 
-    getInput = (event) => {
-        const input = event.target.value;
-        this.props.handleGetInput(input);
-    }
-
-
-    render() {
-        const display = !this.props.expandPreviewer ? 'flex' : 'none';
+        const display = !props.expandPreviewer ? 'flex' : 'none';
         const styles = { display: display };
 
         return (
             <div>
-              <div className={!this.props.expandEditor ? 'editor-container normal-editor' : 'editor-container expanded'} style={styles}>
+              <div className={!props.expandEditor ? 'editor-container normal-editor' : 'editor-container expanded'} style={styles}>
 
                   <div className='editor-header'>
                      <h3>Editor</h3>
-                     {!this.props.expandEditor ?
+                     {!props.expandEditor ?
                      <i className="fas fa-expand-arrows-alt"
-                     onClick={this.props.handleClickEditor}></i>
+                     onClick={props.handleClickEditor}></i>
                      : <i className='fa fa-compress'
-                     onClick={this.props.handleClickEditor}></i>}
+                     onClick={props.handleClickEditor}></i>}
 
 
                     </div>
                     <textarea
                     id='editor'
                     type='text'
-                    value={this.props.value}
-                    onChange={this.getInput}>
+                    value={props.value}
+                    onChange={props.handleGetInput}>
                     </textarea>
                     </div>
                 </div>
         )
-    }
 }
+
+
+export default Editor;
